be01c84522b29117e1f642361e7e1f73939d3085
Paola De Bartolo <paola.debartolo@gmail.com>
2025-02-10 18:09:01 -0300

    feat: version 3.0.0 with Vaadin 24 support
    Uses web-component version 2.0.0 based on lit element.

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
-# Component Factory Breadcrumb for Vaadin 10+
-Breadcrumb is the Java API for [<vcf-breadcrumbs>](https://github.com/vaadin-component-factory/vcf-breadcrumb) web component for Vaadin 10+. Support for Vaadin 14+ is starting from version 2.0.0. 
+# Component Factory Breadcrumb for Vaadin Flow
+Breadcrumb is the Java API for [<vcf-breadcrumbs>](https://github.com/vaadin-component-factory/vcf-breadcrumb) web component for Vaadin Flow. 
 It provides an easy way to display breadcrumb on web pages.
-
-[Live Demo ↗](https://incubator.app.fi/breadcrumb-demo/breadcrumbs)
-
-[<img src="https://raw.githubusercontent.com/vaadin/incubator-breadcrumb/master/screenshot.png" width="400" alt="Screenshot of incubator-breadcrumbs">](https://vaadin.com/directory/components/vaadinincubator-breadcrumbs)
-
 
 ## Usage
 Create instance of `Breadcrumbs` and instances of `Breadcrumb`. You can set breadcrumb text and href. 
-Also you can set property shift, which will indicate whether breadcrumb should be hidden for 
-mobile view(when viewport is smaller then 420px).
+Also you can set property collpase, which will indicate whether breadcrumb should collapse when there's no enough space to display in full.
 ```
 Breadcrumbs breadcrumbs = new Breadcrumbs();
 breadcrumbs.add(
-    new Breadcrumb("Home","breadcrumbs/#", true),
+    new Breadcrumb("Home","breadcrumbs/#"),
     new Breadcrumb("Components", "breadcrumbs/#", true),
     new Breadcrumb("VCF Components", "breadcrumbs/#"),
     new Breadcrumb("Breadcrumbs"));
 ```
-Breadcrumbs "Home" and "Components" will be hidden when viewport is less then 420px  
+Breadcrumb "Components" will collapse and show ellipsis when space available for display is not wide enough.
 
+## Updates since version 3.0.0
+- Vaadin 24.5+ support.
+- Web component part is now Lit based.
+- The first item in the breadcrumb is always shown in full.
+- The items can be collapsed when space runs out. This is configurable by using the attribute `collapse`. When availabe space is not enough to display the full label, then the label is shown with ellipsis.
+- If space is even more limited, and some breadcrumbs have the `collapse` attribute:
+    - Consecutive collapsed items are grouped into ranges.
+    - Each range is hidden when necessary and replaced with an ellipsis element.
+- `shift` attribute from previous version was removed. Responsive behavior is now given by the `collapse` attribute implementation.
 
 ## Setting up for development:
 Clone the project in GitHub (or fork it if you plan on contributing)
 
 ```
-https://github.com/vaadin-component-factory/vcf-breadcrumb
+https://github.com/vaadin-component-factory/breadcrumb
 ```
 
 To build and install the project into the local repository run 
@@ -37,12 +40,11 @@ To build and install the project into the local repository run
 To run demo go to `breadcrumbs-demo/` subfolder and run `mvn jetty:run`.
 After server startup, you'll be able find demo at [http://localhost:8080/breadcrumbs](http://localhost:8080/breadcrumbs)
 
-
 ## License & Author
 
-This Add-on is distributed under Apache 2.0
+This Add-on is distributed under Apache 2.0.
 
-Component Factory Breadcrumb is written by Vaadin Ltd.
+Breadcrumb component is written by Vaadin Ltd.
 
 ### Sponsored development
 Major pieces of development of this add-on has been sponsored by multiple customers of Vaadin. Read more  about Expert on Demand at: [Support](https://vaadin.com/support) and  [Pricing](https://vaadin.com/pricing)
--- breadcrumb-demo/src/main/java/com/vaadin/componentfactory/demo/BreadcrumbView.java
+++ breadcrumb-demo/src/main/java/com/vaadin/componentfactory/demo/BreadcrumbView.java
@@ -2,27 +2,103 @@ package com.vaadin.componentfactory.demo;
 
 import com.vaadin.componentfactory.Breadcrumb;
 import com.vaadin.componentfactory.Breadcrumbs;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
+@SuppressWarnings("serial")
 @Route("breadcrumbs")
 @RouteAlias("")
+@CssImport("./styles/demo-styles.css")
 public class BreadcrumbView extends DemoView {
 
-    @Override
-    protected void initView() {
-        Breadcrumbs breadcrumbs = new Breadcrumbs(
-                new Breadcrumb("Home","breadcrumbs/#", true),
-                new Breadcrumb("Components", "breadcrumbs/#", true),
-                new Breadcrumb("VCF Components", "breadcrumbs/#"),
-                new Breadcrumb("Breadcrumbs"));
+  @Override
+  protected void initView() {
+    basicUseExample();
+    routingExample();
+    collapseExample();
+    collapseEllispisOnlyExample();
+    stylingExample();
+  }
 
-        Div description = new Div();
-        description.setText("Breadcrumbs \"Home\" and \"Components\" will be hidden when " +
-                "viewport size will be smaller then 420px");
+  private void basicUseExample() {
+    Breadcrumbs breadcrumbs = new Breadcrumbs(
+        new Breadcrumb("Home", "breadcrumbs/#"),
+        new Breadcrumb("Components", "breadcrumbs/#", true),
+        new Breadcrumb("VCF Components", "breadcrumbs/#", true), 
+        new Breadcrumb("Breadcrumbs"));
+    Div description = new Div();
+    description.setText("Breadcrumb \"Components\" will be hidden when "
+        + "viewport size will be smaller then 420px.");
+    addCard("Breadcrumbs basic usage", breadcrumbs, description);
+  }
 
-        addCard("Basic Breadcrumbs usage", breadcrumbs, description);
-    }
+  private void routingExample() {
+    Breadcrumbs breadcrumbs = new Breadcrumbs(
+        new Breadcrumb("Home", "hello"),
+        new Breadcrumb("Components", "breadcrumbs/#"),
+        new Breadcrumb("VCF Components", "breadcrumbs/#"), 
+        new Breadcrumb("Breadcrumbs"));
+    Div description = new Div();
+    description.setText("When clicking on \"Home\" it will navigate to a Hello World View.");
+    addCard("Breadcrumbs with routing", breadcrumbs, description);
+  }
+
+  private void collapseExample() {
+    Breadcrumbs breadcrumbs = new Breadcrumbs(
+        new Breadcrumb("Home", "breadcrumbs/#"),
+        new Breadcrumb("Vaadin Flow", "breadcrumbs/#", true),
+        new Breadcrumb("Components", "breadcrumbs/#", true),
+        new Breadcrumb("VCF Components", "breadcrumbs/#", true), 
+        new Breadcrumb("Breadcrumbs"));
+    breadcrumbs.setWidth("350px");
+    Div description = new Div();
+    description.setText("The first item in the breadcrumb is always shown in full. "
+        + "The items with 'collapse' in true are clipped and shown with ellipsis when space runs out.");
+    addCard("Breadcrumbs showing ellipsis", breadcrumbs, description);
+  }
+
+  private void collapseEllispisOnlyExample() {
+    Breadcrumbs breadcrumbs = new Breadcrumbs(
+        new Breadcrumb("Home", "breadcrumbs/#"),
+        new Breadcrumb("Vaadin Flow", "breadcrumbs/#", true),
+        new Breadcrumb("Components", "breadcrumbs/#"),
+        new Breadcrumb("VCF Components", "breadcrumbs/#", true), 
+        new Breadcrumb("Breadcrumbs"));
+    breadcrumbs.setWidth("250px");
+    Div description = new Div();
+    description.setText(
+        "If space is limited, the items with 'collapse' in true are hidden and a single ellipsis element is shown instead."
+            + " The first item is always shown in full.");
+    addCard("Breadcrumbs showing collapse elements replaced with ellipsis element.", breadcrumbs,
+        description);
+  }
+
+  private void stylingExample() {
+    Breadcrumbs breadcrumbs = new Breadcrumbs(
+        createdHomeBreadcrumb("Home", "breadcrumbs/#"),
+        createStyledBreadcrumb("Vaadin Flow", "breadcrumbs/#"),
+        createStyledBreadcrumb("Components", "breadcrumbs/#"),
+        createStyledBreadcrumb("VCF Components", "breadcrumbs/#"), 
+        new Breadcrumb("Breadcrumbs"));
+    Div description = new Div();
+    description.setText("Styles applied to replace default separator with \"/\", "
+        + " change default color of anchor links and make \"Home\" to have a different color "
+        + "than the rest of the breadcrumbs.");
+    addCard("Breadcrumbs with styling", breadcrumbs, description);
+  }
+
+  private Breadcrumb createStyledBreadcrumb(String text, String href) {
+    Breadcrumb breadcrumb = new Breadcrumb(text, href);
+    breadcrumb.addClassName("styled-breadcrumb");
+    return breadcrumb;
+  }
+
+  private Breadcrumb createdHomeBreadcrumb(String text, String href) {
+    Breadcrumb breadcrumb = createStyledBreadcrumb(text, href);
+    breadcrumb.addClassName("home-style");
+    return breadcrumb;
+  }
 }
